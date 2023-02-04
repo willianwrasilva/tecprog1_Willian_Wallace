7bf41a729b0a88b1ea19199f7fd323678e6a6e03
willianwrasilva <willianwra09@gmail.com>
2023-02-04 20:32:17 -0300

    A próxima será a questão 2

--- lista_tecprog1_numpy.py
+++ lista_tecprog1_numpy.py
@@ -6,3 +6,5 @@ vetor = np.random.randint(1, 101, 100)
 print(f'O array gerado é: {vetor}')
 print(f'O valor máximo deste array é: {vetor.max()}')
 print(f'O valor mínimo deste array é: {vetor.min()}')
+
+#Questão 2:
