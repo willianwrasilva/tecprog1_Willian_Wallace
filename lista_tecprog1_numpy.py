88eacce18878694d804802308d45e4f5803c8f21
willianwrasilva <willianwra09@gmail.com>
2023-02-05 18:46:12 -0300

    Questão 1 e 2 resolvidas. Próxima questão a ser resolvida é a 3.

--- lista_tecprog1_numpy.py
+++ lista_tecprog1_numpy.py
@@ -1,13 +1,27 @@
 import numpy as np
 
 #Questão 1:
-
+'''
 vetor = np.random.randint(1, 101, 100)
 print(f'O array gerado é: {vetor}')
 print(f'O valor máximo deste array é: {vetor.max()}')
 print(f'O valor mínimo deste array é: {vetor.min()}')
-
+'''
 #Questão 2:
+quantidade = int(input('Qual a dimensão do vetor: '))
+vetor1 = np.random.randint(1,100, quantidade)
+print(f'O vetor gerado pelo numpy é: {vetor1}')
+
+def modulo_vetor(arr):
+    modulo_quadratico = arr*arr
+    soma = 0
+    for i in modulo_quadratico:
+        soma += i 
+    modulo_vetor = soma**(1/2)
+
+    return modulo_vetor
+
+print(f'O módulo do vetor é: {modulo_vetor(vetor1)}')
 
 #Questão 3:
 
