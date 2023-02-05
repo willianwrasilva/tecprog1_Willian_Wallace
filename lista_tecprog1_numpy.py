a25c37f808b58f0490e60a88a8d668ccc3dca459
willianwrasilva <willianwra09@gmail.com>
2023-02-04 23:47:50 -0300

    Mais uma atualização antes da questão 2

--- lista_tecprog1_numpy.py
+++ lista_tecprog1_numpy.py
@@ -8,3 +8,17 @@ print(f'O valor máximo deste array é: {vetor.max()}')
 print(f'O valor mínimo deste array é: {vetor.min()}')
 
 #Questão 2:
+
+#Questão 3:
+
+#Questão 4:
+
+#Questão 5:
+
+#Questão 6:
+
+#Questão 7:
+
+#Questão 8:
+
+#Questão 9:
