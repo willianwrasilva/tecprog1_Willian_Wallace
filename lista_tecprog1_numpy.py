533f2106116279cda83b6a326c69eca1b528013a
willianwrasilva <willianwra09@gmail.com>
2023-02-07 17:18:37 -0300

    Atualização: Faltam as questões 6, 7 e 11.

--- lista_tecprog1_numpy.py
+++ lista_tecprog1_numpy.py
@@ -14,13 +14,8 @@ vetor1 = np.random.randint(1,100, quantidade)
 print(f'O vetor gerado pelo numpy é: {vetor1}')
 
 def modulo_vetor(arr):
-    modulo_quadratico = arr*arr
-    soma = 0
-    for i in modulo_quadratico:
-        soma += i 
-    modulo_vetor = soma**(1/2)
-
-    return modulo_vetor
+    modulo_quadratico = np.sqrt(sum(arr**2))
+    return modulo_quadratico
 
 print(f'O módulo do vetor é: {modulo_vetor(vetor1)}')
 '''
@@ -29,15 +24,15 @@ print(f'O módulo do vetor é: {modulo_vetor(vetor1)}')
 y_prediction = np.random.randint(1,10, 3)
 y_i = np.random.randint(1,10, 3)
 
+
 print(f'O vetor y_prediction é: {y_prediction}')
 print(f'O vetor y_i é: {y_i}')
 
 def calculate_eqm(arr1, arr2):
-    soma = 0
-    for i in range(len(arr1)):
-        diferenca = (arr1[i] - arr2[i])**2
-        soma += diferenca
-    return soma/len(arr1)
+    diferenca = (arr1 - arr2)**2
+    erro_quadratico_medio = diferenca.sum()/np.size(arr1)
+          
+    return erro_quadratico_medio
 
 print(f'O erro quadrático médio é: {calculate_eqm(y_prediction, y_i)}')
 '''
@@ -46,31 +41,57 @@ print(f'O erro quadrático médio é: {calculate_eqm(y_prediction, y_i)}')
 linhas_4 = int(input('Digite o número de linhas: '))
 colunas_4 = int(input('Digite o número de colunas: '))
 
-matriz_4 = np.ones((linhas_4, colunas_4), dtype= int)
-matriz_4[1:(linhas_4 - 1),1:(colunas_4 - 1)] = 0
+def matriz_borda_um_nucleo_zero(linha, coluna):
+    matriz_4 = np.ones((linha, coluna), dtype= int)
+    matriz_4[1:(linha - 1),1:(coluna - 1)] = 0
+    return matriz_4
 
-print(matriz_4)
+print(f'A matriz é: \n{matriz_borda_um_nucleo_zero(linhas_4, colunas_4)}')
 '''
 #Questão 5:
+'''
 linhas_5 = int(input('Digite o número de linhas: '))
 colunas_5 = int(input('Digite o número de colunas: '))
-linha = np.array(np.zeros(linhas_5, dtype = int))
-coluna = np.array(np.zeros(colunas_5, dtype = int))
 matriz_5 = np.random.randint(1, 21, size = (linhas_5, colunas_5))
-nova_matriz_5 = np.append(matriz_5, [linha], axis = 0)
-nova1_matriz_5 = np.append(nova_matriz_5, [coluna], axis = 1)
 
-print(matriz_5)
-print(nova_matriz_5)
-print(nova1_matriz_5)
+def matriz_borda_zero(matriz):
+    linha_zeros = np.array(np.zeros((1, matriz.shape[1]), dtype = int))
+    coluna_zeros = np.array(np.zeros((matriz.shape[0]+2, 1), dtype = int))
+
+    nova_matriz_5 = np.concatenate((linha_zeros, matriz))
+    nova_matriz_5 = np.concatenate((nova_matriz_5, linha_zeros))
+    nova_matriz_5 = np.concatenate((coluna_zeros, nova_matriz_5), axis = 1)
+    nova_matriz_5 = np.concatenate((nova_matriz_5, coluna_zeros), axis = 1)
+
+    return nova_matriz_5
+
+print(f'Matriz inicial: \n{matriz_5}')
+print(f'Matriz final: \n{matriz_borda_zero(matriz_5)}')
+'''
 #Questão 6:
 
 #Questão 7:
 
 #Questão 8:
-
+'''
+vetor_8 = np.arange(1,22,2)
+print(vetor_8)
+'''
 #Questão 9:
-
+'''
+vetor_9 = np.random.rand(10)
+print(vetor_9)
+'''
 #Questão 10:
+'''
+vetor_10 = np.random.rand(30)
+
+def normaliza_dados(vetor):
+    normalizacao = (vetor - np.mean(vetor))/np.std(vetor)
+    return normalizacao
+
+print(f'O vetor original é: \n {vetor_10}')
+print(f'O vetor normalizado é: \n {normaliza_dados(vetor_10)}')
+'''
 
 #Questão 11:
